74187a53ef1f69efbf483561c60d607b89214655
Michael Mayr <mayr.michael@outlook.com>
2023-12-22 10:47:23 +0100

    added define

--- src/cordic_iterative.v
+++ src/cordic_iterative.v
@@ -1,5 +1,21 @@
-//`include "cordic_slice.v"
+// Copyright 2023 Michael Mayr
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE−2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
 `default_nettype none
+`ifndef __CORDIC_ITERATIVE
+`define __CORDIC_ITERATIVE
+`include "cordic_slice.v"
 
 module cordic_iterative #(
     parameter N_FRAC = 7
@@ -152,3 +168,6 @@ module cordic_iterative #(
 
 
 endmodule
+
+`endif
+`default_nettype wire
