e3abd99b751d820c563f68dce18809033e82079e
Michael Mayr <mayr.michael@outlook.com>
2024-01-05 11:31:28 +0100

    Added cosim for sin_generator, removed Bug from cordic_convergence

--- test/cordic/sin_generator.py
+++ test/cordic/sin_generator.py
@@ -3,7 +3,24 @@ import cordic
 
 def sin_gen(sfixed_fract, phase, iterations, n_samples):
 
+    calc_values, xc_values, yc_values, zc_values = sin_gen_debug(sfixed_fract, phase, iterations, n_samples)
+
+    return calc_values
+
+
+def sin_gen_debug(sfixed_fract, phase, iterations, n_samples):
+
     calc_values = np.zeros(n_samples)
+
+    xc_values = np.zeros(n_samples)
+    yc_values = np.zeros(n_samples)
+    zc_values = np.zeros(n_samples)
+
+    xi_values = np.zeros(n_samples)
+    yi_values = np.zeros(n_samples)
+    zi_values = np.zeros(n_samples)
+
+
 
     lsb = 2**(-sfixed_fract)
     shift_vector = cordic.gen_shift_vector(iterations)
@@ -24,6 +41,10 @@ def sin_gen(sfixed_fract, phase, iterations, n_samples):
         yc = yi
         zc = zi
 
+        xi_values[i] = xc
+        yi_values[i] = yc
+        zi_values[i] = zc
+
         if zi < -0.5:
             zc = zi + 0.5 
             xc = yi
@@ -37,14 +58,15 @@ def sin_gen(sfixed_fract, phase, iterations, n_samples):
         yc = cordic.quantize_value(yc, lsb)
         zc = cordic.quantize_value(zc, lsb)
 
+        xc_values[i] = xc
+        yc_values[i] = yc
+        zc_values[i] = zc
+
         xo, yo, zo = cordic.cordic(xc,yc,zc, angles_vector, shift_vector, iterations, sfixed_fract)
         
         calc_values[i] = yo[iterations]
 
-    return calc_values
-
-
-
+    return calc_values, xc_values, yc_values, zc_values, xi_values, yi_values, zi_values
 
 
 
