95124fc10dd18ff3c2418fad54de17e2e3ba5686
Michael Mayr <mayr.michael@outlook.com>
2024-01-11 18:19:15 +0100

    test if it has place

--- src/tt_um_mayrmichael_cordic.v
+++ src/tt_um_mayrmichael_cordic.v
@@ -37,20 +37,67 @@ module tt_um_mayrmichael_cordic (
     wire enable;
     wire [1:0] waveform;
 
-
-
     assign set_phase_amplitude_value = ui_in;
     
     assign uo_out = data;
 
-    assign uio_oe = 8'b10000000;
-    assign uio_out[7] = data_valid_strobe;
-    assign uio_out[6:0] = 7'b0000000;
+    assign uio_oe = 8'b11100000;
+    assign uio_out[7] = sclk;
+    assign uio_out[6] = sdo;
+    assign uio_out[5] = cs;
+    assign uio_out[4:0] = 5'b00000;
 
     assign enable = uio_in[0];
     assign waveform = uio_in[2:1];
     assign set_phase = uio_in[3];
     assign set_amplitude = uio_in[4];
+
+    wire sclk;
+
+    reg sdo, cs;
+    reg next_sdo, next_cs;
+    reg [2:0] next_counter, counter;
+
+    reg state, next_state;
+
+    always @(posedge clk) begin
+        if (rst_n == 1'b0) begin
+            sdo <= 0;
+            cs <= 1;
+            counter <= 0;
+            state <= 0;
+        end else begin
+            sdo <= next_sdo;
+            cs <= next_cs;
+            counter <= next_counter;
+            state <= next_state;
+        end
+    end
+
+    assign sclk = (state == 1'b1) ? clk : 1'b1;
+
+    always @* begin
+        next_cs = cs;
+        next_sdo = sdo;
+        next_state = state;
+        next_counter = counter;
+
+        if (state == 1'b0) begin
+            if (data_valid_strobe == 1'b1) begin
+                next_cs = 0;
+                next_counter = 0;
+                next_state = 1'b1;
+            end
+        end else begin
+            next_counter = counter + 1;
+            next_sdo = data[counter];
+
+            if (counter == 3'b111) begin
+                next_cs = 1;
+                next_state = 0;
+            end
+        end
+    end
 
     wave_generator wave_generator_inst
     (
